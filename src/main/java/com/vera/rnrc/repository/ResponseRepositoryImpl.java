9c65bb0efe9987223d1d2d331324b7b72afa1919
Vera <work.vera23@mail.ru>
2024-02-21 13:04:39 +0300

    фикс багов

--- src/main/java/com/vera/rnrc/repository/ResponseRepositoryImpl.java
+++ src/main/java/com/vera/rnrc/repository/ResponseRepositoryImpl.java
@@ -10,20 +10,79 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @RequiredArgsConstructor
 public class ResponseRepositoryImpl {
-    private final ResponseRepository responseRepository;
     @PersistenceContext
     private EntityManager entityManager;
 
     @Transactional
     public void createViewForPhysicPerson(@Param("dateList") String dateList, @Param("listName") String listName) {
-        String sql = responseRepository.createViewForPhysicalPerson(dateList, listName);
+        String sql = createViewForPhysicalPerson(dateList, listName);
         entityManager.createNativeQuery(sql).executeUpdate();
     }
 
 
     @Transactional
     public void createViewForLegalPerson(@Param("dateList") String dateList, @Param("listName") String listName) {
-        String sql = responseRepository.createViewForLegalPerson(dateList, listName);
+        String sql = createViewForLegalEntity(dateList, listName);
         entityManager.createNativeQuery(sql).executeUpdate();
+    }
+
+    private String createViewForPhysicalPerson(String dateList, String listName) {
+        String sql = "CREATE or replace VIEW viewPhysical AS " +
+                "SELECT p.id, iop.pzinskey as partner_id " +
+                "FROM index_olr_partnerlinkedstructure iop " +
+                "         JOIN physical_person p ON " +
+                "    p.inn = iop.inn " +
+                "        AND p.listname = '" + listName.replace("'", "''") + "' " +
+                "        AND p.datelist = '" + dateList.replace("'", "''") + "' " +
+                "union " +
+                "SELECT p.id, iop.pzinskey as partner_id " +
+                "FROM index_olr_partnerlinkedstructure iop " +
+                "         JOIN physical_person p ON " +
+                "    p.docseries = iop.docseries AND p.docnumber = iop.docnumber " +
+                "        AND p.listname = '" + listName.replace("'", "''") + "' " +
+                "        AND p.datelist = '" + dateList.replace("'", "''") + "' " +
+                "union " +
+                "SELECT p.id, iop.pzinskey as partner_id " +
+                "FROM index_olr_partnerlinkedstructure iop " +
+                "         JOIN physical_person p ON " +
+                "    p.dateofbirth = iop.dateofbirth " +
+                "        AND LOWER(p.lastname) = LOWER(iop.lastname) AND LOWER(p.firstname) = LOWER(iop.firstname) " +
+                "        AND LOWER(p.middlename) = LOWER(iop.middlename) " +
+                "        AND p.listname = '" + listName.replace("'", "''") + "' " +
+                "        AND p.datelist = '" + dateList.replace("'", "''") + "' " +
+                "union " +
+                "SELECT p.id, iop.pzinskey as partner_id " +
+                "FROM index_olr_partnerlinkedstructure iop " +
+                "         JOIN physical_person p ON " +
+                "    p.dateofbirth = iop.dateofbirth " +
+                "        AND LOWER(p.fullname) = LOWER(iop.fullname) " +
+                "        AND p.listname = '" + listName.replace("'", "''") + "' " +
+                "        AND p.datelist = '" + dateList.replace("'", "''") + "' ;";
+        return sql;
+    }
+
+    private String createViewForLegalEntity(String dateList, String listName) {
+        String sql = "CREATE or replace VIEW viewLegal AS " +
+                "SELECT l.id, r.id as partner_id " +
+                "FROM rnrc_ref_partner r " +
+                "         JOIN legal_person l ON " +
+                "        l.inn = r.inn " +
+                "        AND l.listname = '" + listName.replace("'", "''") + "' " +
+                "        AND l.datelist = '" + dateList.replace("'", "''") + "' " +
+                "union " +
+                "SELECT l.id, r.id as partner_id " +
+                "FROM rnrc_ref_partner r " +
+                "         JOIN legal_person l ON " +
+                "        l.ogrn = r.ogrn " +
+                "        AND l.listname = '" + listName.replace("'", "''") + "' " +
+                "        AND l.datelist = '" + dateList.replace("'", "''") + "' " +
+                "union " +
+                "SELECT l.id, r.id as partner_id " +
+                "FROM rnrc_ref_partner r " +
+                "         JOIN legal_person l ON " +
+                "        l.fullname = r.fullname " +
+                "        AND l.listname = '" + listName.replace("'", "''") + "' " +
+                "        AND l.datelist = '" + dateList.replace("'", "''") + "' ";
+        return sql;
     }
 }
