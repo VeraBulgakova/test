05af621effdf49445b0c8861d64c3d28a4b06282
Vera <work.vera23@mail.ru>
2024-03-21 10:04:38 +0300

    изменение .bat файла все работает

--- startApp.bat
+++ startApp.bat
@@ -4,10 +4,10 @@ SETLOCAL
 cd /d C:\Users\bulga\rnrc
 
 echo Building the project...
-mvn clean package
-pause
+call mvn clean package
+
 echo Running the application...
-java -jar target\partnercheck-0.0.1-SNAPSHOT.jar
+call java -jar target\partnercheck-0.0.1-SNAPSHOT.jar
 
 ENDLOCAL
 pause
